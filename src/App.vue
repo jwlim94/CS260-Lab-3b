189d83941a0b5007a29c3f94bf83aebfd674db81
Jongwoo Lim <limjongwoo1994@gmail.com>
2020-10-30 23:45:40 -0600

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,81 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="menu">
+      <div id="brand">
+        <router-link to="/">
+          <img src="/images/logo.png">
+        </router-link>
+      </div>
+      <div id="side">
+        <router-link to="/browse">
+          <div class="menu-item browse">
+            <img src="/images/globe.png">
+            <p>Browse</p>
+          </div>
+        </router-link>
+        <router-link to="/cart">
+          <div class="menu-item">
+            <img src="/images/love.png">
+            <p>0 items</p>
+          </div>
+        </router-link>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+body {
+  margin: 50px 100px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr; /* 3등분; 1fr: each one taking equal fraction of width of the screen */
+  grid-column-gap: 5px; /* 등분 간의 간격 */
+  grid-template-areas: "none brand side"; /* 등분 각각 이름 */
+  margin-bottom: 50px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#menu a {
+  color: #B84901; /* HTML의 a는 여기에서 router-link를 가리킴 */
+}
+
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+
+#brand img {
+  height: 200px;
+}
+
+#side {
+  grid-area: side;
+  display: flex;
+  justify-content: flex-end;
+}
+
+#side img {
+  width: 50px;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: column; /* 이미지와 문자열이 열(column; 위,아래)로 표현되게 함 (default: 옆으로 나란히(row)) */
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+.browse {
+  margin-right: 50px;
 }
 </style>
---
+++ src/views/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ProductList :products="products" />
+    </div>
+</template>
+
+<script>
+import ProductList from "../components/ProductList.vue"
+export default {
+  name: 'Cart',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    products() {
+      return this.$root.$data.cart;
+    }
+  },
+}
+</script>
